5f7a33a6385b65725faa341b56befbe51a183263
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-03-14 12:20:37 +0100

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../assets/quiz-logo.png'
+
+const Header = () => {
+    return (
+        <header>
+            <img src={Logo} />
+            <h2>Quiz App</h2>
+        </header>
+    )
+}
+
+export default Header;
