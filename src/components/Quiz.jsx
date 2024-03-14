55fc1e75af6b711a333c1908e1d893517a70487f
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-03-14 18:34:07 +0100

    update Quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,13 +4,16 @@ import QUESTIONS from '../questions'
 const Quiz = () => {
     const [reponses, setReponses] = useState([]);
     const currentQuestion = reponses.length;
+    const HandleSelectAnswer = (answer) => {
+        setReponses(prev => ([...prev, answer]))
+    }
     return (
         <div id="question">
             <h2>{QUESTIONS[currentQuestion].text}</h2>
             <ul id="answers">
                 {QUESTIONS[currentQuestion].answers.map(qst => (
                     <li key={qst} className="answer">
-                        <button>{qst}</button>
+                        <button onClick={() => { HandleSelectAnswer(qst) }}>{qst}</button>
                     </li>
                 ))}
             </ul>
