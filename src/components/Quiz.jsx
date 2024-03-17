292aeb6e1189b5a1ddb1a47233d4240765d377ab
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-03-17 14:48:03 +0100

    Shuffling Answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,6 +4,8 @@ import QUESTIONS from '../questions'
 const Quiz = () => {
     const [reponses, setReponses] = useState([]);
     const currentQuestion = reponses.length;
+    const newTab = [...QUESTIONS[currentQuestion].answers];
+    newTab.sort(() => Math.random() - 0.5);
     const HandleSelectAnswer = (answer) => {
         setReponses(prev => ([...prev, answer]))
     }
@@ -12,7 +14,7 @@ const Quiz = () => {
             <div id="question">
                 <h2>{QUESTIONS[currentQuestion].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[currentQuestion].answers.map(qst => (
+                    {newTab.map(qst => (
                         <li key={qst} className="answer">
                             <button onClick={() => { HandleSelectAnswer(qst) }}>{qst}</button>
                         </li>
