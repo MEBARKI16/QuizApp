55ae689617d2c0923f398be6bba2fe55ecd703e2
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-03-17 17:17:22 +0100

    Adding Quiz Logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,29 +1,45 @@
 import React, { useState } from 'react';
-import QUESTIONS from '../questions'
+import QUESTIONS from '../questions';
+import completed from "../assets/quiz-complete.png";
 
 const Quiz = () => {
     const [reponses, setReponses] = useState([]);
     const currentQuestion = reponses.length;
-    const newTab = [...QUESTIONS[currentQuestion].answers];
-    newTab.sort(() => Math.random() - 0.5);
-    const HandleSelectAnswer = (answer) => {
-        setReponses(prev => ([...prev, answer]))
-    }
-    return (
-        <div id="quiz">
-            <div id="question">
-                <h2>{QUESTIONS[currentQuestion].text}</h2>
-                <ul id="answers">
-                    {newTab.map(qst => (
-                        <li key={qst} className="answer">
-                            <button onClick={() => { HandleSelectAnswer(qst) }}>{qst}</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
+    const newTab = QUESTIONS[currentQuestion]?.answers
+    ? [...QUESTIONS[currentQuestion].answers].sort(() => Math.random() - 0.5)
+    : [];
+  
 
-    )
-}
+    const handleSelectAnswer = (answer) => {
+        setReponses(prev => ([...prev, answer]));
+    };
+
+    const quizIsComplete = (reponses.length === QUESTIONS.length);
+    console.log(quizIsComplete);
+
+    return (
+        <>
+            {quizIsComplete ?
+                <div id="summary">
+                    <img src={completed} alt="Quiz Completed!" />
+                    <h2>Quiz Completed!</h2>
+                </div>
+                :
+                <div id="quiz">
+                    <div id="question">
+                        <h2>{QUESTIONS[currentQuestion]?.text}</h2>
+                        <ul id="answers">
+                            {newTab.map((qst, index) => (
+                                <li key={index} className="answer">
+                                    <button onClick={() => handleSelectAnswer(qst)}>{qst}</button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            }
+        </>
+    );
+};
 
 export default Quiz;
