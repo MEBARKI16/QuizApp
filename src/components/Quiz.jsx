e2cede99d13dca43d2bc1547f3ff624aa959b65f
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-03-28 11:36:08 +0100

    Highlighting Selected Answers & Managing More State

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,8 +5,8 @@ import QuestionTimer from './QuestionTimer';
 
 const Quiz = () => {
     const [reponses, setReponses] = useState([]);
-    console.log(reponses)
-    const currentQuestion = reponses.length;
+    const [answerState, setAnswerState] = useState("")
+    const currentQuestion = answerState === "" ? reponses.length : reponses.length - 1;
     const newTab = QUESTIONS[currentQuestion]?.answers
         ? [...QUESTIONS[currentQuestion].answers].sort(() => Math.random() - 0.5)
         : [];
@@ -14,7 +14,18 @@ const Quiz = () => {
 
     const handleSelectAnswer = useCallback((answer) => {
         setReponses(prev => ([...prev, answer]));
-    }, []);
+        setAnswerState("selected")
+        setTimeout(() => {
+            if (answer === QUESTIONS[currentQuestion].answers[0]) {
+                setAnswerState("correct");
+            }
+            else { setAnswerState("wrong"); }
+            setTimeout(() => {
+                setAnswerState("")
+            }, 2000);
+        }, 1000)
+        console.log(answerState)
+    }, [currentQuestion]);
     const onTimeOut = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
     const quizIsComplete = (reponses.length === QUESTIONS.length);
     console.log(quizIsComplete);
@@ -32,11 +43,13 @@ const Quiz = () => {
                         <QuestionTimer key={QUESTIONS[currentQuestion]?.text} timeOut={10000} onTimeOut={onTimeOut} />
                         <h2>{QUESTIONS[currentQuestion]?.text}</h2>
                         <ul id="answers">
-                            {newTab.map((qst, index) => (
-                                <li key={index} className="answer">
-                                    <button onClick={() => handleSelectAnswer(qst)}>{qst}</button>
+                            {newTab.map((qst, index) => {
+                                const isSelected = reponses[reponses.length - 1] === qst;
+                                const style = isSelected ? answerState : "";
+                                return <li key={index} className="answer">
+                                    <button className={style} onClick={() => handleSelectAnswer(qst)}>{qst}</button>
                                 </li>
-                            ))}
+                            })}
                         </ul>
                     </div>
                 </div>
